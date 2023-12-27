bd6f7b56e3ad15fc60df98a3ced7c715d2d4e88b
Stefan <euler90h@gmail.com>
2023-12-27 10:59:50 -0500

    Challenge 4 solution.

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"log"
 	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -19,13 +19,77 @@ type ReadMessage struct {
 	Type string
 }
 
+type GossipMessage struct {
+	Type string
+	Counters map[string]int
+}
+
+type InitMessage struct {
+	Type string `json:"type"`
+	NodeID string `json:"node_id"`
+	NodeIDs []string `json:"node_ids"`
+	MsgID int `json:"msg_id"`
+}
+
 type GCounter struct {
+	// maps node ids to counters.
+	counters map[string]int
+	// true if we've updated counters but have yet to propagate state, false otherwise.
+	state_dirty bool
+	neighbors []string
 	node *maelstrom.Node
-	kv *maelstrom.KV
+	//kv *maelstrom.KV
 	mu sync.Mutex
 }
 
-const COUNTER_KEY = "counter"
+func (gc *GCounter) NewGossipMessage() GossipMessage {
+	msg := GossipMessage{}
+	msg.Type = "gossip"
+	gc.mu.Lock()
+	msg.Counters = gc.counters
+	gc.mu.Unlock()
+	return msg
+}
+
+func (gc *GCounter) PropagateState() {
+	msg := gc.NewGossipMessage()
+	gc.Multicast(msg)
+	gc.mu.Lock()
+	gc.state_dirty = false
+	gc.mu.Unlock()
+}
+
+func (gc *GCounter) Multicast(msg interface{}) {
+	gc.mu.Lock()
+	if gc.node == nil {
+		panic("me is nil")
+	}
+	for _, node := range gc.neighbors {
+		if node != gc.node.ID() {
+			err := gc.node.RPC(node, msg, gc.HandleGossipResponse)
+			if err != nil {
+				log.Printf("problem sending message to node %v\n", node)
+			}
+		}
+	}
+	gc.mu.Unlock()
+}
+
+func (gc *GCounter) HandleReadMessage(raw_msg maelstrom.Message) error {
+	msg := ReadMessage{}
+	if err := json.Unmarshal(raw_msg.Body, &msg); err != nil {
+		
+		return err
+	}
+	if msg.Type != "read" {
+		return errors.New("HandleReadMessage tried to process non-read message!")
+	}
+	v := gc.Value()
+	response := make(map[string]any)
+	response["type"] = "read_ok"
+	response["value"] = v
+	return gc.node.Reply(raw_msg, response)
+}
 
 func (gc *GCounter) HandleAddMessage(raw_msg maelstrom.Message) error {
 	msg := AddMessage{}
@@ -36,103 +100,137 @@ func (gc *GCounter) HandleAddMessage(raw_msg maelstrom.Message) error {
 		return errors.New("HandleAddMessage tried to process non-add message!")
 	}
 
-	ctx := context.Background()
-
-	gc.mu.Lock()
-	defer gc.mu.Unlock()
-	r, err := gc.kv.Read(ctx, COUNTER_KEY)
-	v := 0
-	if err != nil {
-		var rpcError *maelstrom.RPCError
-		if errors.As(err, &rpcError) {
-			if rpcError.Code == maelstrom.KeyDoesNotExist {
-				gc.kv.Write(ctx, COUNTER_KEY, 0)
-				v = 0
-			} else {
-				return err
-			}
-		} else {
-			return err
-		}
-	} else {
-		v = r.(int)
-	}
-	log.Printf("%v + %d = %v\n", v, msg.Delta, v + msg.Delta)
-	err = gc.kv.Write(ctx, COUNTER_KEY, v + msg.Delta)
-	if err != nil {
-		return err
-	}
-	
+	gc.Add(msg.Delta)
 	response := make(map[string]any)
 	response["type"] = "add_ok"
 	return gc.node.Reply(raw_msg, response)
 }
 
+func (gc *GCounter) HandleGossipMessage(msg maelstrom.Message) error {
+	body := GossipMessage{}
+	if err := json.Unmarshal(msg.Body, &body); err != nil {		
+		return err
+	}
+	
+	if err := gc.Merge(body.Counters); err != nil {
+		return err
+	}
+
+	reply := make(map[string]any)
+	reply["type"] = "gossip_ok"
+
+	return gc.node.Reply(msg, reply)
+}
+
+func (gc *GCounter) HandleGossipResponse(msg maelstrom.Message) error {
+	body := GossipMessage{}
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	log.Printf("node %v recv'd gossip response from node %v\n", msg.Dest, msg.Src)
+	return nil
+}
+
 func (gc *GCounter) HandleNodeInit(raw_msg maelstrom.Message) error {
-	ctx := context.Background()
+	init_msg := InitMessage{}
+	if err := json.Unmarshal(raw_msg.Body, &init_msg); err != nil {
+		return err
+	}
+
 	gc.mu.Lock()
 	defer gc.mu.Unlock()
-	_, err := gc.kv.Read(ctx, COUNTER_KEY)
-	if err == nil {
-		response := make(map[string]any)
-		response["type"] = "init_ok"
-		return gc.node.Reply(raw_msg, response)
-	}
-	var rpcError *maelstrom.RPCError
-	if errors.As(err, &rpcError) {
-		if rpcError.Code == maelstrom.KeyDoesNotExist {
-			gc.kv.Write(ctx, COUNTER_KEY, 0)
-		} else {
-			return err
-		}
-	}
+
+	gc.neighbors = init_msg.NodeIDs
+	log.Println(gc.neighbors)
+
 	response := make(map[string]any)
 	response["type"] = "init_ok"
 	return gc.node.Reply(raw_msg, response)
 }
 
-func (gc *GCounter) HandleReadMessage(raw_msg maelstrom.Message) error {
-	msg := ReadMessage{}
-	if err := json.Unmarshal(raw_msg.Body, &msg); err != nil {
-		return err
-	}
-	if msg.Type != "read" {
-		return errors.New("HandleReadMessage tried to process non-read message!")
-	}
-	ctx := context.Background()
+func (gc *GCounter) Compare(other map[string]int) bool {
 	gc.mu.Lock()
 	defer gc.mu.Unlock()
-	v, err := gc.kv.Read(ctx, COUNTER_KEY)
-	if err != nil {
-		var rpcError *maelstrom.RPCError
-		if errors.As(err, &rpcError) {
-			if rpcError.Code == maelstrom.KeyDoesNotExist {
-				gc.kv.Write(ctx, COUNTER_KEY, 0)
-				v = 0
-			} else {
-				return err
-			}
-		} else {
-			return err
+
+	for k := range gc.counters {
+		if gc.counters[k] > other[k] {
+			return false
 		}
 	}
-	response := make(map[string]any)
-	response["type"] = "read_ok"
-	response["value"] = v.(int)
-	return gc.node.Reply(raw_msg, response)
+
+	return true
+}
+
+func (gc *GCounter) Merge(other map[string]int) error {
+	gc.mu.Lock()
+	defer gc.mu.Unlock()
+	if len(other) != len(gc.counters) {
+		return errors.New("len(other) != len(gc.counters)")
+	}
+
+	log.Printf("node %v pre-merge: %v\n", gc.node.ID(), gc.counters)
+
+	for k := range gc.counters {
+		gc.counters[k] = max(gc.counters[k], other[k])
+	}
+
+	log.Printf("node %v post-merge: %v\n", gc.node.ID(), gc.counters)
+
+	return nil
+}
+
+func (gc *GCounter) Add(k int) {
+	gc.mu.Lock()
+	defer gc.mu.Unlock()
+	gc.counters[gc.node.ID()] += k
+	log.Printf("node %v added %d -- it is now %d\n", gc.node.ID(), k, gc.counters[gc.node.ID()])
+	gc.state_dirty = true
+}
+
+func (gc *GCounter) Value() int {
+	gc.mu.Lock()
+	defer gc.mu.Unlock()
+	v := 0
+	for k := range gc.counters {
+		v += gc.counters[k]
+	}
+	return v
+}
+
+func (gc *GCounter) HandleTicker() {
+	gc.mu.Lock()
+	if gc.state_dirty {
+		gc.mu.Unlock()
+		gc.PropagateState()
+	}
 }
 
 func main() {
-	n := maelstrom.NewNode()
 	gc := GCounter{}
-	gc.node = n
-	gc.kv = maelstrom.NewSeqKV(n)
+	gc.node = maelstrom.NewNode()
+	//gc.kv = maelstrom.NewSeqKV(gc.node)
+	gc.counters = make(map[string]int)
+	gc.neighbors = make([]string, 8)
 
-	n.Handle("init", gc.HandleNodeInit)
-	n.Handle("add", gc.HandleAddMessage)
-	n.Handle("read", gc.HandleReadMessage)
+	gc.node.Handle("init", gc.HandleNodeInit)
+	gc.node.Handle("add", gc.HandleAddMessage)
+	gc.node.Handle("read", gc.HandleReadMessage)
+	gc.node.Handle("gossip", gc.HandleGossipMessage)
 
-	if err := n.Run(); err != nil {
+	ticker := time.NewTicker(1 * time.Second)
+    done := make(chan bool)
+    go func() {
+        for {
+            select {
+            case <-done:
+                return
+            case _ = <-ticker.C:
+				gc.HandleTicker()
+            }
+        }
+    }()
+
+	if err := gc.node.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
